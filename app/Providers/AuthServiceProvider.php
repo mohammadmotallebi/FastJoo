c116b251e2f25522fa6558002702d6db1d57de5d
Mohammad Motalebi <mohamad.motalebi@gmail.com>
2023-10-23 17:54:06 -0600

    Update 23-oct-2023

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,17 +22,22 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
-        foreach (\App\Models\Ability::all() as $ability) {
-            \Gate::define($ability->ability.'-'.$ability->table_name, function (User $user){
-                $abilities = $user->abilities();
-                return $user->hasAbility($abilities)
-                    ? Response::allow()
-                    : Response::deny('You must be an administrator.');
-            });
+
+        // check mysql db connection is available
+        try {
+                $this->registerPolicies();
+                foreach (\App\Models\Ability::all() as $ability) {
+                    \Gate::define($ability->ability . '-' . $ability->table_name, function (User $user) {
+                        $abilities = $user->abilities();
+                        return $user->hasAbility($abilities)
+                            ? Response::allow()
+                            : Response::deny('You must be an administrator.');
+                    });
+                }
+
+        } catch (\Exception $e) {
+            // do nothing
         }
-
-
 
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
